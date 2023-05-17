646afef581793077a8d95f1eb9da94815934015e
pro100pava <pro100pava@gmail.com>
2023-05-17 20:43:08 +0300

    first commit

---
+++ LearnJS/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// const result = confirm("Are you hear?");
+// console.log(result);
+
+
+// const answer = prompt("you have 18 age", "18");
+// console.log(answer + 10);
+
+
+// const answers = [];
+//     answers[0] = prompt('What your name?', '');
+//     answers[1] = prompt('What your Surname?', '');
+//     answers[2] = prompt('What your nage?', '');
+// document.write(answers);
+
+
+const category = 'toys';
+const user = "Paha";
+alert(`Hi, ${user}`);
+
+
